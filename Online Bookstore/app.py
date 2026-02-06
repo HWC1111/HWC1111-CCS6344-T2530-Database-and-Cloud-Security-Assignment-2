98c2164abb04f88db9ea86b23a3777cf7576e072
Beef <114460055+beef1016@users.noreply.github.com>
2026-02-06 22:26:52 +0800

    Update app.py

--- Online Bookstore/app.py
+++ Online Bookstore/app.py
@@ -368,5 +368,7 @@ def admin_order_details(order_id):
 
 # MAIN
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    # This enables HTTPS
+    app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
+
 
